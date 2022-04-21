ed6bb36126d3ccb1e168dc596cb62b2dfe31fa34
Brooklyn Robertson <brooklynwynonsmith@gmail.com>
2022-04-22 02:37:56 +1200

    did so much...
    started again with css from scratch, mobile first. using flex to help with responsiveness. it was great. did som eformatting and file management.
    
    'newcss' is the old css, using the current css stylesheet for final stylings.

--- UX CS101/script.js
+++ UX CS101/script.js
@@ -1,23 +1,27 @@
 // nav function to show links on mobile with hamburger link
 function hamburgerClick() {
     const navIcon = document.querySelector('.nav');
-  
-    if(navIcon.style.display == 'none') {
-      navIcon.style.display = 'flex';
+
+    if (navIcon.style.display == 'none') {
+        navIcon.style.display = 'flex';
     } else {
-      navIcon.style.display = 'none';
+        navIcon.style.display = 'none';
     }
-  }
+}
+
+//function to show pop up of active deals
+function activeDeals() {
+    console.log("active deals placeholder");
+}
 
 
-
-
+//carousel
 var i;
-var slidePosition =0;
+var slidePosition = 0;
 SlideShow(slidePosition);
 
 function slidesPlus(i) {
-    SlideShow(slidePosition+=i);
+    SlideShow(slidePosition += i);
 }
 
 function currentSlide(i) {
@@ -25,25 +29,25 @@ function currentSlide(i) {
 }
 
 function SlideShow() {
-    var slides= document.getElementsByClassName("carouselslides");
-    var slideMarker= document.getElementsByClassName("currentslidemarker");
-    if (i>slideMarker.length) {
-        slidePosition =1;
+    var slides = document.getElementsByClassName("carouselslides");
+    var slideMarker = document.getElementsByClassName("currentslidemarker");
+    if (i > slideMarker.length) {
+        slidePosition = 1;
     }
-    if (i<1) {
+    if (i < 1) {
         slidePosition = slides.length;
     }
-    for (i=0;i<slides.length;i++){
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slidePosition++;
-    if (slidePosition >slides.length) {
+    if (slidePosition > slides.length) {
         slidePosition = 1;
     }
-    for(i=0;i<slideMarker.length;i++){
+    for (i = 0; i < slideMarker.length; i++) {
         slideMarker[i].className = slideMarker[i].className.replace(" enable", "");
     }
-    slides[slidePosition-1].style.display ="flex";
-    slideMarker[slidePosition-1].className += " enable";
-    setTimeout(SlideShow,2000);
+    slides[slidePosition - 1].style.display = "flex";
+    slideMarker[slidePosition - 1].className += " enable";
+    setTimeout(SlideShow, 2000);
 }
