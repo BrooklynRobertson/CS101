ff16b7f69b52991e911dfcd8d1641de432df8a07
Brooklyn Robertson <brooklynwynonsmith@gmail.com>
2022-04-22 23:56:38 +1200

    doing this on vs code, finally finished home page

--- UX CS101/script.js
+++ UX CS101/script.js
@@ -1,53 +1,71 @@
 // nav function to show links on mobile with hamburger link
 function hamburgerClick() {
-    const navIcon = document.querySelector('.nav');
-
-    if (navIcon.style.display == 'none') {
-        navIcon.style.display = 'flex';
-    } else {
-        navIcon.style.display = 'none';
+  const navButtons = document.querySelector(".nav");
+console.log(navButtons);
+  
+    if (navButtons.style.display == "none") {
+        navButtons.style.display = "flex";
+    } 
+    else {
+    navButtons.style.display = "none";
     }
+
+
 }
 
 //function to show pop up of active deals
 function activeDeals() {
-    console.log("active deals placeholder");
+  console.log("active deals placeholder");
 }
 
-
 //carousel
-var i;
-var slidePosition = 0;
-SlideShow(slidePosition);
+
+let slideMarker = document.getElementsByClassName("currentslidemarker");
+let i;
+let slidePosition = 0;
+let intervalId;
+
+automateSlides(SlideShow(slidePosition));
 
 function slidesPlus(i) {
-    SlideShow(slidePosition += i);
+  SlideShow((slidePosition += i));
 }
 
 function currentSlide(i) {
-    SlideShow(slidePosition = i);
+  SlideShow((slidePosition = i));
+}
+
+function automateSlides(slideshow) {
+  // check if already an interval has been set up
+  if (!intervalId) {
+    intervalId = setInterval(SlideShow, 2000);
+  }
 }
 
 function SlideShow() {
-    var slides = document.getElementsByClassName("carouselslides");
-    var slideMarker = document.getElementsByClassName("currentslidemarker");
-    if (i > slideMarker.length) {
-        slidePosition = 1;
-    }
-    if (i < 1) {
-        slidePosition = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slidePosition++;
-    if (slidePosition > slides.length) {
-        slidePosition = 1;
-    }
-    for (i = 0; i < slideMarker.length; i++) {
-        slideMarker[i].className = slideMarker[i].className.replace(" enable", "");
-    }
-    slides[slidePosition - 1].style.display = "flex";
-    slideMarker[slidePosition - 1].className += " enable";
-    setTimeout(SlideShow, 2000);
+  const slides = Array.from(document.getElementsByClassName("carouselslides"));
+
+  if (i > slideMarker.length) {
+    slidePosition = 1;
+  }
+  if (i < 1) {
+    slidePosition = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slidePosition++;
+
+  if (slidePosition > slides.length) {
+    slidePosition = 1;
+  }
+
+  for (i = 0; i < slideMarker.length; i++) {
+    slideMarker[i].className = slideMarker[i].className.replace(" enable", "");
+  }
+
+  slides[slidePosition - 1].style.display = "inline";
+  slideMarker[slidePosition - 1].className += " enable";
+  
 }
